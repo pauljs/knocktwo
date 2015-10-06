032022606b04c0ec801ffd03aa66c146322da71a
Justin Paul <jspaul94@gmail.com>
2015-10-06 18:41:21 -0500

    sessions

--- web.rb
+++ web.rb
@@ -2,6 +2,23 @@ require 'rubygems'
 require 'twilio-ruby'
 require 'sinatra'
 
-get '/' do
-  'Hello World! Currently running version ' + Twilio::VERSION + ' of the twilio-ruby library.'
+enable :sessions
+
+#get '/' do
+#  'Hello World! Currently running version ' + Twilio::VERSION + ' of the twilio-ruby library.'
+#end
+
+get '/sms-quickstart' do
+  session["counter"] ||= 0
+  sms_count = session["counter"]
+  if sms_count == 0
+    message = "Hello, thanks for the new message."
+  else
+    message = "Hello, thanks for message number #{sms_count + 1}"
+  end
+  twiml = Twilio::TwiML::Response.new do |r|
+    r.Message message
+  end
+  session["counter"] += 1
+  twiml.text
 end
