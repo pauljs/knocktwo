42b7a0de58a7a01a8f676c9bb41880b338eaeb89
Justin Paul <jspaul94@gmail.com>
2015-10-13 21:41:33 -0500

    test

--- web.rb
+++ web.rb
@@ -24,20 +24,14 @@ get '/sms-quickstart' do
   session[params[:From]]["counter"] ||= 0
   sms_count = session[params[:From]]["counter"]
 
-  if response == "get stuff"
-    #message = session[params[:From]].to_s
-    message = "test"
-    twiml = Twilio::TwiML::Response.new do |r|
-      r.Message "Info: " + message
-    end
-    twiml.text
-    return
-  end
 
   START = 0
   FIRST_RESPONSE = 1
   SECOND_RESPONSE = 2
-  if sms_count == START
+  if response == "get stuff"
+     session[params[:From]]["counter"] -= 1
+    message = "test"
+  elsif sms_count == START
     message = "Hello! This is Knock, your personal health tracker assistant from your doctor. Please answer the following question in whole numbers.\nHow many total hours of sleep did you get last night? (e.g. 8)"
   elsif sms_count == FIRST_RESPONSE
     if all_digits? response
@@ -45,14 +39,14 @@ get '/sms-quickstart' do
       session[params[:From]]["response"] = response
     else
       message = "Sorry, your response was not in the correct format. I received:\n" + response + "\nbut expected a whole number. Please answer the following question in whole numbers.\nHow many total hours of sleep did you get last night? (e.g. 8)"
-      session["counter"] -= 1 
+     session[params[:From]]["counter"] -= 1 
     end
 
   elsif sms_count == SECOND_RESPONSE
     if all_letters? response
       response = response.downcase
       if response == 'yes'
-        if session["response"].to_i >= 7
+        if session[params[:From]]["response"].to_i >= 7
           suggestion = "Glad to see you are sleeping enough!"
         else
           suggestion = "You should try to get more sleep."
@@ -67,18 +61,18 @@ get '/sms-quickstart' do
 
       elsif response == 'no'
         message = "Please resend your response to the following question in whole numbers.\nHow many total hours of sleep did you get last night? (e.g. 8)"
-        session["counter"] -= 2
+        session[params[:From]]["counter"] -= 2
       else
         message = "Sorry, your response was not in the correct format. I received:\n" + response + "\nbut expected Yes or No. Please state Yes or No."
-        session["counter"] -= 1
+        session[params[:From]]["counter"] -= 1
       end
     else
       message = "Sorry, your response was not in the correct format. I received:\n" + response + "\nbut expected Yes or No. Please state Yes or No."
-        session["counter"] -= 1
+        session[params[:From]]["counter"] -= 1
     end
   elsif sms_count > SECOND_RESPONSE && response.downcase == "edit"
     message = "Hello! This is Knock, your personal health tracker assistant from your doctor. Please answer the following question in whole numbers.\nHow many total hours of sleep did you get last night? (e.g. 8)"
-    session["counter"] = 0
+    session[params[:From]]["counter"] = 0
   else
     message = "You have completed this task. If you would like to edit your response, respond with Edit; otherwise, I'll let you know when you have another task!"
   end
